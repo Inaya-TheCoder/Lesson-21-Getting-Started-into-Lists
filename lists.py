d6b39288b19a6d82ef99e3be467b877361b8a926
Inaya-TheCoder <inaya.singh2013@gmail.com>
2026-01-13 21:22:07 +0530

    LISTS

---
+++ lists.py
@@ -0,0 +1,12 @@
+emptylist = []
+print (emptylist)
+
+number = [1,2,4,3,6,7,8,9,5]
+print (number)
+
+triples = (1,2,3)*3
+print (triples)
+
+listA = [500,400,300,200,100]
+listB = listA[::-1]
+print (listB)
